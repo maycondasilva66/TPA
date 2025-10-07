a4275792fc1e8690f997b314a419c38d32ae38be
maycondasilva66 <maycon.silvaetec66@gmail.com>
2025-10-07 11:22:07 -0300

    Add files via upload

---
+++ PrjAula1/AreaTriangulo.java
@@ -0,0 +1,17 @@
+package prjAula01;
+
+public class AreaTriangulo {
+	public static void main(String[] args) {
+		//área triângulo
+		int base, altura, area;
+		
+		//atribuindo valores
+		base = 4;
+		altura = 7;
+		
+		area = base*altura/2;
+		System.out.println("O resultado é ="+area);
+	    
+	}
+
+}
---
+++ PrjAula1/SomaDois.java
@@ -0,0 +1,17 @@
+package prjAula01;
+
+public class SomaDois {
+	public static void main(String[] args) {
+		//declaração variáveis
+		int valor1, valor2, soma;
+		
+		//atribuindo valores
+		valor1 = 5;
+		valor2 = 42;
+		
+		soma = valor1+valor2;
+		System.out.println("A soma é = "+soma);
+		
+	}
+
+}
