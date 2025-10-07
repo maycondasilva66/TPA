bce27fad644dfdf83ed970eefa486dad5c6dee51
maycondasilva66 <maycon.silvaetec66@gmail.com>
2025-10-07 11:27:46 -0300

    Add files via upload

---
+++ PrjAula002/diasMesesAnos.java
@@ -0,0 +1,12 @@
+package prjAula02;
+import java.util.Scanner;
+
+public class diasMesesAnos {
+	public static void main(String[] args) {
+		//criando um scanner chamado ler
+		Scanner ler = new Scanner(System.in);
+		
+		int dia, 
+	}
+
+}
