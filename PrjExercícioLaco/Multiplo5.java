d6b9d3e9f8557611f713c8ae6664cd0489270d88
maycondasilva66 <maycon.silvaetec66@gmail.com>
2025-10-10 18:20:09 -0300

    Add files via upload

---
+++ PrjExercícioLaco/Multiplo5.java
@@ -0,0 +1,10 @@
+package prjExercicioLaco;
+public class Multiplo5 {
+	public static void main(String[] args) {
+		int n=0,i;
+		for (i=0;i<=400;i+=5) {
+			n=n+i;
+		}
+		System.out.println(n);
+	}
+}
