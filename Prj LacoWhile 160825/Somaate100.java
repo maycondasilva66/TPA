7d48a2b66015fee602afec1273e40d12070a0512
maycondasilva66 <maycon.silvaetec66@gmail.com>
2025-10-10 19:09:33 -0300

    Add files via upload

---
+++ Prj LacoWhile 160825/Somaate100.java
@@ -0,0 +1,16 @@
+package prjLacoRepeticao;
+import java.util.Scanner;
+public class Somaate100 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner (System.in);
+		int i=1;
+		int soma=0;
+		while (i<=100) {
+			soma +=i;
+			i++;
+		}
+		System.out.println("A soma dos números de 1 a 100 é igual a: "+soma);
+		in.close();
+	}
+
+}
