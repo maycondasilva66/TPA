e4db042fc95d53901360d7d5f8e1ed3908f1a6f9
Maycon da Silva <maycon.silvaetec66@gmail.com>
2025-11-23 19:06:50 -0300

    Add files via upload

---
+++ Entrada, Saída e Processamento/Aula do dia 290425/Idade.java
@@ -0,0 +1,17 @@
+package prjAula01;
+import java.
+public class Idade {
+	public static void main(String[] args) {
+		//idade
+		int anoNascimento, anoAtual, idade;
+		
+		//atribuindo valores
+		anoNascimento;
+		anoAtual;
+		
+		
+		
+		
+	}
+
+}
